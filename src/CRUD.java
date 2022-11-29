c443439d76d8cb5904a70fdd8d081643602d7297
mdevos <martadevos@iesnervion.es>
2022-11-29 10:28:07 +0100

    25/11/2022

--- src/CRUD.java
+++ src/CRUD.java
@@ -1,8 +1,6 @@
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CRUD {
@@ -20,17 +18,60 @@ public class CRUD {
         try {
             st.executeUpdate(sql.toString());
         } catch (SQLException e) {
-            System.out.println("Hubo un error al enviar los datos a la base de datos");
+            System.out.println("El registro ya existe");
         }
     }
 
-    public static void borrarEnTabla(Statement st, String valorId, String campoId, String tabla) {
-        StringBuilder sql = new StringBuilder("DELETE FROM ad2223_cmendoza." + tabla + " WHERE " + campoId + " LIKE " + valorId);
+    public static void bloquearDesbloquearContactos(Statement st, String usuarioConectado) {
+        Scanner s = new Scanner(System.in);
+        StringBuilder sql = new StringBuilder("UPDATE ad2223_cmendoza.Contactos SET bloqueado=");
+        String idUsuario2;
+        int bloqueado = 2;
+
+        System.out.println("Introduce el usuario a bloquear/desbloquear");
+        idUsuario2 = s.nextLine();
 
         try {
+            ResultSet rs = st.executeQuery("SELECT * FROM ad2223_cmendoza.Contactos WHERE idUsuario1 LIKE '" + usuarioConectado + "' AND idUsuario2 LIKE '" + idUsuario2 +"'");
+
+            ResultSetMetaData md = rs.getMetaData();
+            while (rs.next()) {
+                bloqueado = rs.getInt(md.getColumnLabel(3));
+            }
+            if (bloqueado==0) {
+                bloqueado = 1;
+            }else if(bloqueado==1){
+                bloqueado = 0;
+            }
+
+            if (bloqueado!=2){
+            sql.append(bloqueado);
+
+            sql.append(" WHERE idUsuario1 LIKE '").append(usuarioConectado).append("' AND idUsuario2 LIKE '").append(idUsuario2).append("'");
+
             st.executeUpdate(sql.toString());
+
+            System.out.println("Se ha bloqueado al usuario " + idUsuario2 + " en tus contactos");
+            }else System.out.println("El usuario" + idUsuario2 + "no existe");
         } catch (SQLException e) {
-            System.out.println("Hubo un error al enviar los datos a la base de datos");
+            System.out.println("Hubo un error al borrar los datos de la base de datos");
+        }
+    }
+
+    public static void borrarEnTablaContactos(Statement st, String usuarioConectado) {
+        Scanner s = new Scanner(System.in);
+        String idUsuario2;
+
+        System.out.println("Introduce el usuario a borrar");
+        idUsuario2 = s.nextLine();
+
+        String sql = "DELETE FROM ad2223_cmendoza.Contactos WHERE idUsuario1 LIKE '" + usuarioConectado + "' AND idUsuario2 LIKE '" + idUsuario2 +"'";
+
+        try {
+            st.executeUpdate(sql);
+            System.out.println("Se ha borrado el usuario " + idUsuario2 + " de tu lista de contactos");
+        } catch (SQLException e) {
+            System.out.println("Hubo un error al borrar los datos de la base de datos");
         }
     }
 
@@ -49,25 +90,37 @@ public class CRUD {
             case 2 -> {
                 campos.add("idUsuario1");
                 campos.add("idUsuario2");
-                //datos.add("idUsuario1"); BUSCAR LA FORMA DE SABER EL USUARIO QUE ESTA USANDO EL PROGRAMA
+                datos.add("'"+idUsuarioIniciado+"'");
                 pedirDatos(datos, 2);
                 tabla = "Contactos";
+                if (Objects.equals(datos.get(0), datos.get(1))){
+                    System.out.println("No puedes añadirte a ti mismo como contacto ;b");
+                    fallo = true;
+                }
             }
-            case 3 -> {
-                campos.add("idEmisor");
-                campos.add("idReceptor");
-                campos.add("texto");
-                //datos.add("idEmisor"); BUSCAR LA FORMA DE SABER EL USUARIO QUE ESTA UTILIZANDO LA APP
-                //datos.add("idReceptor"); BUSCAR LA FORMA DE SABER EL USUARIO QUE RECIBE EL MENSAJE
-                pedirDatos(datos, 3);
-                tabla = "Mensaje";
-            }
+
             default -> {
                 fallo = true;
                 System.out.println("se ha producio un error");
             }
         }
         if (!fallo) insertarEnTabla(st, campos, datos, tabla);
+    }
+
+    public static void enviarMensaje(Statement st, int opc, String idUsuarioIniciado, String idReceptor) {
+        Scanner sc = new Scanner(System.in);
+        String tabla = "";
+        ArrayList<String> campos = new ArrayList<>();
+        ArrayList<String> datos = new ArrayList<>();
+        campos.add("idEmisor");
+        campos.add("idReceptor");
+        campos.add("texto");
+        datos.add(idUsuarioIniciado);
+        datos.add(idReceptor);
+        System.out.println("Introduzca su mensaje:");
+        datos.add("'" + sc.nextLine() + "'");
+        tabla = "Mensaje";
+        insertarEnTabla(st, campos, datos, tabla);
     }
 
     public static ArrayList<String> pedirDatos(ArrayList<String> datos, int opc) {
@@ -94,39 +147,82 @@ public class CRUD {
     public static String iniciarSesion(Statement st) {
         Scanner s = new Scanner(System.in);
         String usuario, contrasena;
-        boolean iniciarSesion = true;
-        do {
-            System.out.println("Introduce tu usuario");
-            usuario = s.nextLine();
+        boolean salir = false;
+        System.out.println("Introduce tu usuario");
+        usuario = s.nextLine();
+        if (!selectTablaUsuario(st, "nombreUsuario", usuario)) {
+            usuario = "";
+            System.out.println("El usuario introducido no existe");
+            Menus.menuInicio(st);
+            salir = true;
+        }
+        while (!salir) {
             System.out.println("Introduce tu contraseña");
             contrasena = s.nextLine();
-            if (!selectTablaUsuario(st, "nombreUsuario", usuario)){
-                System.out.println("El usuario introducido no existe, inténtelo de nuevo");
-                iniciarSesion = false;
-            }else if (!selectTablaUsuario(st, "contrasena", contrasena)){
+            if (!selectTablaUsuario(st, "contrasena", contrasena)) {
                 System.out.println("La contraseña introducido no es correcta, inténtelo de nuevo");
-                iniciarSesion = false;
-            }
-        } while (!iniciarSesion);
+            }else salir = true;
+        }
         return usuario;
     }
 
     /**
      * Método que hace un Select de la taba usuario comprobando que el registro con el dato introducido existe
+     *
      * @param campo campo al que corresponde el dato que se introduce
-     * @param dato dato para filtrar el select
+     * @param dato  dato para filtrar el select
      * @return un booleano verdadero si se encuentran registros con el dato introducido y falso si no se encuentran registros
      */
     public static boolean selectTablaUsuario(Statement st, String campo, String dato) {
         boolean usuarioEncontrado;
         ResultSet rs;
         try {
-            String sql = "SELECT * FROM ad2223_cmendoza.Usuario WHERE " + campo + " LIKE " + dato;
+            String sql = "SELECT * FROM ad2223_cmendoza.Usuario WHERE " + campo + " LIKE '" + dato + "'";
             rs = st.executeQuery(sql);
-            usuarioEncontrado = rs.getRow() != 0;
+            usuarioEncontrado = true;
+        } catch (SQLException e) {
+            usuarioEncontrado = false;
+        }
+        return usuarioEncontrado;
+    }
+
+    public static void selectMostrarTablaContactos(Statement st, String usuarioConectado) {
+        int bloqueado;
+        String cadena = "";
+        ResultSet rs;
+        System.out.println("----------LISTA DE CONTACTOS----------");
+        try {
+            String sql = "SELECT * FROM ad2223_cmendoza.Contactos WHERE idUsuario1 LIKE '" + usuarioConectado + "'";
+            rs = st.executeQuery(sql);
+
+            ResultSetMetaData md = rs.getMetaData();
+            while (rs.next()) {
+                cadena = rs.getString(md.getColumnLabel(2));
+                bloqueado = rs.getInt(md.getColumnLabel(3));
+                if (bloqueado == 1) cadena += " -> BLOQUEADO";
+                System.out.println(cadena);
+            }
+            if (Objects.equals(cadena, "")) System.out.println("No hay contactos");
+            System.out.println("--------------------------------------");
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return usuarioEncontrado;
+    }
+
+    public static void mostrarDatos(ResultSet rs) {
+        try {
+            ResultSetMetaData md = rs.getMetaData();
+            while (rs.next()) {
+                StringBuilder cadena = new StringBuilder();
+                for (int i = 1; i <= md.getColumnCount(); i++) {
+                    cadena.append(md.getColumnLabel(i).toUpperCase()).append(": ").append(rs.getString(md.getColumnLabel(i))).append("    ");
+                }
+                System.out.println(cadena);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: Clase CentroSalud, método mostrarDatos");
+            e.printStackTrace();
+        }
     }
 }
--- src/Menus.java
+++ src/Menus.java
@@ -1,4 +1,5 @@
 import java.sql.Statement;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Menus {
@@ -16,11 +17,14 @@ public class Menus {
         int opc;
         mostrarMenuInicio();
         opc = s.nextInt();
-        switch (opc){
-            case 1 -> CRUD.recogerDatosEInsertar(st, 1, idUsuarioIniciado);
+        switch (opc) {
+            case 1 -> {
+                CRUD.recogerDatosEInsertar(st, 1, idUsuarioIniciado);
+                menuInicio(st);
+            }
             case 2 -> {
                 idUsuarioIniciado = CRUD.iniciarSesion(st);
-                menuPrincipal(st, idUsuarioIniciado);
+                if (!Objects.equals(idUsuarioIniciado, "")) menuPrincipal(st, idUsuarioIniciado);
             }
             case 3 -> System.out.println("¡Gracias por utilizar Whatsapp2!\n¡Hasta luego! ;)");
         }
@@ -28,8 +32,8 @@ public class Menus {
 
     public static void mostrarMenuPrincipal() {
         System.out.println("Elija una opción:" +
-                "\n 1. Ver contactos. " +
-                "\n 2. Ver mensajes pendientes."+
+                "\n 1. Ver menu contactos. " +
+                "\n 2. Ver mensajes pendientes." +
                 "\n 3. Añadir contacto." +
                 "\n 4. Salir.");
     }
@@ -37,39 +41,52 @@ public class Menus {
     public static void menuPrincipal(Statement st, String idUsuarioIniciado) {
         Scanner s = new Scanner(System.in);
         int opc;
-        mostrarMenuInicio();
+        mostrarMenuPrincipal();
         opc = s.nextInt();
-        switch (opc){
-            case 1 -> System.out.println("");//Llamada a método para mostrar contactos
+        switch (opc) {
+            case 1 -> menuContacto(st, idUsuarioIniciado);
             case 2 -> System.out.println("");//Llamada a método mostrar mensajes pendientes
-            case 3 -> CRUD.recogerDatosEInsertar(st, 2, idUsuarioIniciado);
+            case 3 -> {
+                CRUD.recogerDatosEInsertar(st, 2, idUsuarioIniciado);
+                menuPrincipal(st, idUsuarioIniciado);
+            }
             case 4 -> System.out.println("¡Gracias por utilizar Whatsapp2!\n¡Hasta luego! ;)");
         }
     }
 
 
-
     public static void mostrarMenuContacto() {
         System.out.println("Elija una opción:" +
-                "\n 1. Ver/Iniciar conversación. " +
-                "\n 2. Bloquear/Desbloquear" +
-                "\n 3. Borrar contacto"+
-                "\n 4. Volver a menú principal" +
-                "\n 5. Salir.");
+                "\n 1. Ver lista contactos" +
+                "\n 2. Iniciar conversación. " +
+                "\n 3. Bloquear/Desbloquear" +
+                "\n 4. Borrar contacto" +
+                "\n 5. Ver conversacion" +
+                "\n 6. Volver a menú principal" +
+                "\n 7. Salir.");
     }
 
-    public static void menuContacto(Statement st) {
+    public static void menuContacto(Statement st, String idUsuarioIniciado) {
         Scanner s = new Scanner(System.in);
-        int opc = 0;
-        mostrarMenuInicio();
+        int opc, seguir = 0;
+        mostrarMenuContacto();
         opc = s.nextInt();
-        switch (opc){
-            case 1 -> System.out.println("");//Llamada a método para mostrar contactos
-            case 2 -> System.out.println(""); //Llamada a método mostrar mensajes pendientes
-            case 3 -> CRUD.recogerDatosEInsertar(st, 2);
-            case 4 -> menuPrincipal(st);
-            case 5 -> System.out.println("¡Gracias por utilizar Whatsapp2!\n¡Hasta luego! ;)");
+        switch (opc) {
+            case 1 -> CRUD.selectMostrarTablaContactos(st, idUsuarioIniciado);
+            case 2 -> System.out.println("");//Llamada metodo iniciar conver
+            case 3 -> CRUD.bloquearDesbloquearContactos(st, idUsuarioIniciado);
+            case 4 -> CRUD.borrarEnTablaContactos(st, idUsuarioIniciado);
+            case 5 -> System.out.println("");//Llamada metodo ver conver
+            case 6 -> {
+                menuPrincipal(st, idUsuarioIniciado);
+                seguir = 1;
+            }
+            case 7 -> {
+                System.out.println("¡Gracias por utilizar Whatsapp2!\n¡Hasta luego! ;)");
+                seguir = 1;
+            }
         }
+        if (seguir == 0) menuContacto(st, idUsuarioIniciado);
     }
 
 }
